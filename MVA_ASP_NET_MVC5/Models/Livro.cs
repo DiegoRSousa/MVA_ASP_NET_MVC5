b781bd5c4abc66f9ae6cdc660f8cc7b2d892bd15
Diego Sousa <diegors90@gmail.com>
2015-07-19 10:57:23 -0300

    Model e controller

---
+++ MVA_ASP_NET_MVC5/Models/Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVA_ASP_NET_MVC5.Models
+{
+    public class Livro
+    {
+        [Key]
+        public int IDLivro { get; set; }
+
+        [Required(ErrorMessage="Digite o titulo do livro")]
+        [StringLength(100, ErrorMessage="O tamanho máximo de caracteres é {1}", MinimumLength=3)]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Digite o autor do livro")]
+        public int Autor { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Estoque { get; set; }
+
+        public bool Disponivel { get; set; }
+    }
+}
