59a2d9671ec67433f8eb0aecda11646f2daf087c
Diego Sousa <diegors90@gmail.com>
2015-07-19 00:20:41 -0300

    O que e um controller?

---
+++ MVA_ASP_NET_MVC5/Controllers/OlaMundoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVA_ASP_NET_MVC5.Controllers
+{
+    public class OlaMundoController : Controller
+    {
+        // GET: OlaMundo
+        public string Index()
+        {
+            return "Olá MVC 5";
+        }
+
+        public string TesteController()
+        {
+            return "Texto <b>em negrito</b>, retorno do controller";
+        }
+
+        // GET: OlaMundo/GetJson
+        public JsonResult GetJson()
+        {
+            var produto = new 
+            {
+                ID = 12,
+                Nome = "Bicicleta",
+                Qtde = 10
+            };
+            return Json(produto, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: OlaMundo/GetDados?nome=Diego&idade=24
+        public string GetDados(string nome, int idade = 20)
+        {
+            return HttpUtility.HtmlEncode(
+                string.Format("Bem Vindo {0} com idade {1}", nome, idade));
+        }
+    }
+}
