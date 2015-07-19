cde98fd42beb602e583baaf5b8bc150700beb12c
Diego Sousa <diegors90@gmail.com>
2015-07-18 23:50:11 -0300

    Introducao a arquitetura MVC

---
+++ MVA_ASP_NET_MVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVA_ASP_NET_MVC5.MvcApplication" Language="C#" %>
---
+++ MVA_ASP_NET_MVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVA_ASP_NET_MVC5.Startup))]
+namespace MVA_ASP_NET_MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
